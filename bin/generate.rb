fc388ccb020ca4ff63058df5189c1a06b62e5b84
Erik Schierboom <erik_schierboom@hotmail.com>
2020-08-25 16:47:24 +0200

    Support output directory argument

--- bin/generate.rb
+++ bin/generate.rb
@@ -1,3 +1,3 @@
 load File.expand_path('../../lib/representer.rb', __FILE__)
 
-Representer.generate(ARGV[0], ARGV[1])
+Representer.generate(ARGV[0], ARGV[1], ARGV[2])
--- lib/represent_solution.rb
+++ lib/represent_solution.rb
@@ -1,18 +1,18 @@
 class RepresentSolution
   include Mandate
 
-  initialize_with :exercise_slug, :path
+  initialize_with :exercise_slug, :solution_path, :output_path
 
   def call
-    code = File.read(path / "#{exercise_slug.tr('-', '_')}.rb")
+    code = File.read(solution_path / "#{exercise_slug.tr('-', '_')}.rb")
     representation = Representation.new(code)
     representation.normalize!
 
-    File.open(path / "representation.txt","w") do |f|
+    File.open(output_path / "representation.txt","w") do |f|
       f.write(representation.ast)
     end
 
-    File.open(path / "mapping.json","w") do |f|
+    File.open(output_path / "mapping.json","w") do |f|
       f.write(representation.mapping.to_json)
     end
   end
